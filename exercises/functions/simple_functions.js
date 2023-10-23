62fcb50cf5fbfe22b6b5d1436e18125cf49da3f6
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-23 15:25:59 -0400

    exercise 1

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,23 @@
+function displayFavoriteThings() {
+  console.log("Craig");
+  console.log("Seven");
+  console.log("A Tribe Called Quest");
+}
+
+displayFavoriteThings();
+console.log();
+
+function displayWhyImHere() {
+  console.log("Because is fun and actualizing.");
+}
+
+displayWhyImHere();
+console.log();
+
+function displayFavoritePlace() {
+  console.log("I love New York.");
+}
+
+displayFavoritePlace();
+
+
