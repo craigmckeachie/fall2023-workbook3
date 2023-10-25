3cb65deec78ef077dc7bcbca9ad23de2346fc9e6
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-25 09:43:03 -0400

    back to original

--- demos/1-20.js
+++ demos/1-20.js
@@ -9,10 +9,10 @@ function decrement() {
   count = count - 1;
 }
 
-increment();
-increment();
-increment();
-increment();
-decrement();
+increment(); //1
+increment(); //2
+increment(); //3
+increment(); //4
+decrement(); //3
 
 console.log(count);
