7bb77b5a8061509a9ffc7cb737daec222cb90c0d
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-25 08:39:40 -0400

    params ordering

--- demos/parameters.js
+++ demos/parameters.js
@@ -1,14 +1,11 @@
+function displayFavoriteThings(name, movie, band) {
+    console.log("Name: " + name);
+    console.log("Movie: " + movie);
+    console.log("Band: " + band);
+}
 
 // displayFavoriteThings("Craig", "Seven", "A Tribe Called Quest");
 // displayFavoriteThings("A Tribe Called Quest", "Seven", "Craig" );
 // displayFavoriteThings(1, 2, 3)
 
 displayFavoriteThings("c", "a", "b" );
-
-
-
-function displayFavoriteThings(name, movie, band) {
-    console.log("Name: " + name);
-    console.log("Movie: " + movie);
-    console.log("Band: " + band);
-}
--- demos/parameters1.js
+++ demos/parameters1.js
@@ -1,13 +1,3 @@
-// displayFavoriteThings("Craig", "Seven", "A Tribe Called Quest");
-// displayFavoriteThings("A Tribe Called Quest", "Seven", "Craig" );
-// displayFavoriteThings(1, 2, 3)
-
-let variable1 = "a";
-let variable2 = "b";
-let variable3 = "c";
-
-displayFavoriteThings(variable1, "d", variable3);
-
 function displayFavoriteThings(name, movie, band) {
   let message = ` 
                 ${name}
@@ -21,3 +11,13 @@ function displayFavoriteThings(name, movie, band) {
   // console.log("Movie: " + movie);
   // console.log("Band: " + band);
 }
+
+// displayFavoriteThings("Craig", "Seven", "A Tribe Called Quest");
+// displayFavoriteThings("A Tribe Called Quest", "Seven", "Craig" );
+// displayFavoriteThings(1, 2, 3)
+
+let variable1 = "a";
+let variable2 = "b";
+let variable3 = "c";
+
+displayFavoriteThings(variable1, "d", variable3);
