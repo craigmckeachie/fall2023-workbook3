709facbc49d129a58a11767fcc0a295d19284469
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-25 09:54:14 -0400

    update scope demos

---
+++ demos/1-20-local.js
@@ -0,0 +1,10 @@
+let anotherName = "Brad";
+
+function showName() {
+  let name1 = "Sarita";
+  console.log(name1);
+}
+
+showName();
+// console.log(name1); //ReferenceError: name1 is not defined
+console.log(anotherName);
