cfc327bdf03fbf7fb12f00632f40a208f24f94b3
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-25 16:00:10 -0400

    objects and basic date demos

---
+++ demos/dates.js
@@ -0,0 +1,4 @@
+let myDate = new Date(2023, 4, 31)
+console.log(myDate.toString());
+// let currentMonth = myDate.getMonth();
+// console.log(currentMonth);
---
+++ demos/objects.js
@@ -0,0 +1,33 @@
+let first = "Tristan";
+let last = "Tran";
+let hairColor = "black";
+let wearsGlasses = true;
+
+function draw() {}
+
+function drum() {}
+
+//-----------------
+
+let tristan = {
+  first: "Tristan",
+  last: "Tran",
+  hairColor: "black",
+  wearsGlasses: true,
+  draw: function () {},
+  drum: function () {},
+};
+
+date = {
+  now: function () {},
+  getMonth: function () {},
+  toString: function(){
+    
+  }
+};
+
+let console = {
+  log: function () {},
+};
+
+console.log();
